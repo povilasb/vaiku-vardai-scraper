2a2d447fca402f161945a712aea1b74769cd3aa9
Povilas Balciunas <balciunas90@gmail.com>
2017-04-23 20:38:00 +0300

    Update export format to UTF-8

--- vardai_scraper/settings.py
+++ vardai_scraper/settings.py
@@ -4,3 +4,4 @@ USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 ' \
     '(KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 CONCURRENT_REQUESTS = 1
 DOWNLOAD_DELAY = 2.5
+FEED_EXPORT_ENCODING = 'utf-8'
